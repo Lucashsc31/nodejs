17c5e6621d46d43cd2f80bd2ac2c3185d8d07f07
logonrm <logonrm@fiap.com.br>
2017-08-04 21:30:33 -0300

    criando exemplo app

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
 var express = require("express");
 var app = express();
 
+//ROUTES
+
+//inicial
 app.get('/', function(req, res){
     res.send("<html><body>Página Inicial</body></html>");
+});
+
+//tecnologia
+app.get('/tecnologia', function(req, res){
+    res.send("<html><body>Página de Técnologia</body></html>");
+});
+
+//musica
+app.get('/musica', function(req, res){
+    res.send("<html><body>Página de Musica</body></html>");
+});
+
+//musica
+app.get('/tabuada', function(req, res){
+    var tabuadaHTML = [];
+    tabuadaHTML.push("<center>");
+    tabuadaHTML.push("<h2>Tabuadas</h2>");
+    for(i = 0; i <= 10; i++){
+        tabuadaHTML.push("<hr>");
+        tabuadaHTML.push("<b>Tabuada do " + i + "</b>");
+        for(j = 0; j <= 10; j++){
+            tabuadaHTML.push("<br>");
+            tabuadaHTML.push(i + "x" + j + "=" + (i*j));
+        }   
+    }
+    tabuadaHTML.push("</center>");
+    res.send("<html><body>" + tabuadaHTML.join("") + "</body></html>");
+});
+
+app.listen(3000, function(){
+    console.log("Servidor Ativo, rodando com Express!");
 });
