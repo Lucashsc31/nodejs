d2022158229231466b51a74418d9bc91d6230250
logonrm <logonrm@fiap.com.br>
2017-08-07 20:15:11 -0300

    Rotas basicas criadas

--- app.js
+++ app.js
@@ -1,39 +1,48 @@
 var express = require("express");
 var app = express();
 
+//A engine de view sera EJS, e para setarmos isso na propriedade node
+app.set('view engine', 'ejs');
+
 //ROUTES
 
 //inicial
 app.get('/', function(req, res){
-    res.send("<html><body>Página Inicial</body></html>");
+    res.render('home/home');
 });
 
-//tecnologia
-app.get('/tecnologia', function(req, res){
-    res.send("<html><body>Página de Técnologia</body></html>");
+app.get('/cadastrar', function(req, res){
+    res.render('admin/form_add_produto');
 });
 
-//musica
-app.get('/musica', function(req, res){
-    res.send("<html><body>Página de Musica</body></html>");
+app.get('/produtos', function(req, res){
+    res.render('produtos/produtos');
 });
 
-//musica
-app.get('/tabuada', function(req, res){
-    var tabuadaHTML = [];
-    tabuadaHTML.push("<center>");
-    tabuadaHTML.push("<h2>Tabuadas</h2>");
-    for(i = 0; i <= 10; i++){
-        tabuadaHTML.push("<hr>");
-        tabuadaHTML.push("<b>Tabuada do " + i + "</b>");
-        for(j = 0; j <= 10; j++){
-            tabuadaHTML.push("<br>");
-            tabuadaHTML.push(i + "x" + j + "=" + (i*j));
-        }   
-    }
-    tabuadaHTML.push("</center>");
-    res.send("<html><body>" + tabuadaHTML.join("") + "</body></html>");
-});
+
+// app.get('/tecnologia', function(req, res){
+//     res.send("<html><body>Página de Técnologia</body></html>");
+// });
+
+// app.get('/musica', function(req, res){
+//     res.send("<html><body>Página de Musica</body></html>");
+// });
+
+// app.get('/tabuada', function(req, res){
+//     var tabuadaHTML = [];
+//     tabuadaHTML.push("<center>");
+//     tabuadaHTML.push("<h2>Tabuadas</h2>");
+//     for(i = 0; i <= 10; i++){
+//         tabuadaHTML.push("<hr>");
+//         tabuadaHTML.push("<b>Tabuada do " + i + "</b>");
+//         for(j = 0; j <= 10; j++){
+//             tabuadaHTML.push("<br>");
+//             tabuadaHTML.push(i + "x" + j + "=" + (i*j));
+//         }   
+//     }
+//     tabuadaHTML.push("</center>");
+//     res.send("<html><body>" + tabuadaHTML.join("") + "</body></html>");
+// });
 
 app.listen(3000, function(){
     console.log("Servidor Ativo, rodando com Express!");
