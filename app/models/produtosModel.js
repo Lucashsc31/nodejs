439b39ea1de14b1fecfd934d125760c8d5183306
logonrm <logonrm@fiap.com.br>
2017-09-01 21:38:00 -0300

    .

--- app/models/produtosModel.js
+++ app/models/produtosModel.js
@@ -5,5 +5,8 @@ module.exports = function(){
     this.getProduto = function(connection, callback){
         connection.query('SELECT * FROM produtos WHERE id_produtos = 1', callback);
     }
+    this.salvarProduto = function(produto, connection, callback){
+        connection.query('INSERT INTO produtos SET ?', produto, callback);
+    }
     return this;
 }
--- app/routes/cadastrar.js
+++ app/routes/cadastrar.js
@@ -5,6 +5,7 @@ module.exports = function(app){
 
     app.post('/produtos/salvar',function(req, res){
         var produto = req.body;
+        var connection = app.config.dbConnection();
         res.send(produto);
     });
 }
